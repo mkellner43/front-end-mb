468523bda46d4def4457b40b41cddd566740cee8
mkellner43 <matthew.kellner@outlook.com>
2023-05-03 09:06:21 -0500

    loading port with env vars

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(() => {
+export default defineConfig(({mode}) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())}
   return {
     build: {
       outDir: 'build',
     },
     plugins: [react()],
     server: {
-      port: 3001
+      port: parseInt(process.env.VITE_PORT)
     }
   };
 });
